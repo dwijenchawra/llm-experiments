d296e7ce2ac2dd47fd98ab9322f7bc09d288c15e
Dwijen Chawra <dwchawra@gmail.com>
2023-05-22 20:31:10 -0400

    minor fixes

--- regularpytorch/jsoncleaner.py
+++ regularpytorch/jsoncleaner.py
@@ -18,4 +18,3 @@ with open('/scratch/gilbreth/dchawra/sgpt_nonempty.json', 'w') as file:
     json.dump(new_data, file, indent=4)
 
 
-torchrun --nproc_per_node=3 --master_port=20001 train/train_mem.py --model_name_or_path /scratch/gilbreth/dchawra/vicuna-7b  --data_path /scratch/gilbreth/dchawra/sgpt_nonempty.json --bf16 True --output_dir /scratch/gilbreth/dchawra/vicuna-ft --num_train_epochs 3 --per_device_train_batch_size 2 --per_device_eval_batch_size 2 --gradient_accumulation_steps 16 --evaluation_strategy "no" --save_strategy "steps" --save_steps 1200 --save_total_limit 10 --learning_rate 2e-5 --weight_decay 0. --warmup_ratio 0.03 --lr_scheduler_type "cosine" --logging_steps 1 --fsdp "full_shard auto_wrap offload" --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' --tf32 True --model_max_length 2048 --gradient_checkpointing True --lazy_preprocess True
