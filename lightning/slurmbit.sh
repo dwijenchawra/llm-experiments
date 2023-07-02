e489e2a3f6d49e111f747332f24a26fdf02f5b1e
Dwijen Chawra <dwchawra@gmail.com>
2023-07-02 13:37:59 -0400

    slurm files

--- lightning/slurmbit.sh
+++ lightning/slurmbit.sh
@@ -1,10 +1,12 @@
-#!/bin/bash -l
+#!/bin/bash
 
 # SLURM SUBMIT SCRIPT
 #SBATCH --nodes=2
+#SBATCH --account=euge-k
+#SBATCH --partition=gilbreth-k
 #SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=32
 #SBATCH --ntasks-per-node=2
-#SBATCH --mem=0
 #SBATCH --time=0-02:00:00
 
 # activate conda env
@@ -20,6 +22,7 @@ export PYTHONFAULTHANDLER=1
 
 # might need the latest CUDA
 # module load NCCL/2.4.7-1-cuda.10.0
+module load cuda/12.1.0
 
 # run script from above
-srun python3 multnodetest.py
+srun python multnodetest.py
